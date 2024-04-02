b912a13c1a3ad2f3b993d9a70b6e888870937e5e
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-02 07:27:23 -0700

    defined basic schema for flight model

---
+++ models/flight.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+
+
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date
+
+}, {timestamps: true});
+
+// complile the schema into a model, and export it
+
+module.exports = mongoose.model('Flight', flightSchema);
