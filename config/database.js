c31b227918fbb6464c888394dae3fff0a473a6f8
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-02 07:24:25 -0700

    Added database connection setup using mongoose

---
+++ config/database.js
@@ -0,0 +1,15 @@
+// require mongoose
+const mongoose =  require('mongoose');
+
+// establish connection to mongoDb
+mongoose.connect(process.env.DATABASE_URL);
+
+// connected event listener
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+
+    console.log(`connected to MongDB ${db.name} on ${db.host}: ${db.port}`)
+
+});
