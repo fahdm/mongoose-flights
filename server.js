02bf3557c4a97ab20a0183fcc396d265e83689cc
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-03 19:44:52 -0700

    configured desitnation router

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ require('./config/database'); // run database connection
 
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
+var destinationRouter = require('./routes/destinations');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
+app.use('/',  destinationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
